e06924adfab6f9edfb9295d0bc77b39cb61b7cb6
Grundrak <electrovilla66@gmail.com>
2023-10-03 12:18:14 +0100

    push blog

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const blogPost = [
     {
         id: 1,
         title: 'Test post',
-        picture: 'AA.jpg',
+        img: 'dj.jpg',
+        text: 'i wanna try to see this text on my blog',
+      },
+      {
+        id: 2,
+        title: 'Test post 2',
+        img: 'dj.jpg',
+        text: 'i wanna try to see this text on my blog',
+      },
+      {
+        id: 3,
+        title: 'Test post 3',
+        img: 'dj.jpg',
         text: 'i wanna try to see this text on my blog',
       },
 ]
@@ -15,15 +27,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/post',(req,res) =>{
     res.render('blog', {blogPost});
 });
-app.get('/post',(req,res) =>{
-    res.send('post')
-});
 
+app.get("/post/:id", (req, res) => {
+    let post = blogPost.find(
+      (post) => post.id === parseInt(req.params.id)
+    );
+    if (post) {
+      res.render('postDetails',{post : post});
+    } else {
+      res.status(404).send();
+    }
+  });
+  app.delete("/post/:id", (req, res) => {
+    let post = blogPost.find(
+      (post) => post.id === parseInt(req.params.id)
+    );
+    if (post !== -1) {
+     blogPost.splice(post, 1);
+      res.Status(200).send('The blog as delete with succces');
+    } else {
+      res.Status(404).send('Sorry but the blog didint delete');
+    }
+  });
+app.post('/post',(req,res) =>{
+    const addPost = {id: blogPost.lenght++,title, img ,text}
+    blogPost.push(addPost);
+    if(addPost){
+        res.status(201).send("Done the new post added")
+    }else {
+        res.status(400).send("Error in post repost please")
+    }
+}); 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
